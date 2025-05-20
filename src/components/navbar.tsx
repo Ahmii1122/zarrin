e1ca91f6da1896cd45bef9281e05c23eb2d243a5
ahmii1122 <asam4055144@gmail.com>
2025-05-20 22:39:41 +0500

    refactored

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,10 +2,9 @@ import { useState, useEffect } from "react";
 import logo from "../assets/Logo.png";
 import { CiSearch } from "react-icons/ci";
 import { HiMiniBars3BottomRight } from "react-icons/hi2";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
-  const Navigate = useNavigate();
   const location = useLocation();
   const [isOpen, setIsOpen] = useState(false);
   const [active, setActive] = useState<string | null>(null);
